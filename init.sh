ee83f43b2e7b6bc3a5c96911327d28c20dcf4428
VMdardy <82144070+VMdardy@users.noreply.github.com>
2021-05-21 11:58:47 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,5 @@
-sudo pip install django-bootstrap3
-
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-
-sudo rm /etc/gunicorn.d/hello.py
-sudo rm /etc/gunicorn.d/django.py
-
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
-
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+﻿sudo /etc/init.d/mysql start﻿
