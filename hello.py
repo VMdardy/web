f6cc4b9d05e0f4f1af75783cec68f5297df6367b
VMdardy <82144070+VMdardy@users.noreply.github.com>
2021-06-17 12:03:34 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 def app(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return  ['Hello, world!']
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    return body
